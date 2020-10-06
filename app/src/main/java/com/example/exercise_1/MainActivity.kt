ecf47c074ace01d7073b7c624e6d97199b03cb2d
mnithyanandh <71746055+mnithyanandh@users.noreply.github.com>
2020-10-06 07:55:05 +0530

    added second activity and fixed main activity

--- app/src/main/java/com/example/exercise_1/MainActivity.kt
+++ app/src/main/java/com/example/exercise_1/MainActivity.kt
@@ -4,8 +4,9 @@ import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import android.content.Intent;
+import android.widget.Button;
+import android.widget.EditText;
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +15,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
 
+//      Creating tags for holding values of different user variables
+        val FullName = findViewById<EditText>(R.id.Full_Name_Entry)
+        val Qualification = findViewById<EditText>(R.id.Qualification_Entry)
+        val Profession = findViewById<EditText>(R.id.Profession_Entry)
+        val Hobby = findViewById<EditText>(R.id.Hobby_Entry)
+        val DreamJob = findViewById<EditText>(R.id.Dream_Job_Entry)
+
+//      Creating tag for the 'Submit' Button
+        val Submit = findViewById<Button>(R.id.Submit)
+
+//      Declaring 'Submit' button intent. Also convert the user variables to Strings to pass down to second activity for display
+        Submit.setOnClickListener {
+//          String conversion of user data:
+            val fullname = FullName.text.toString()
+            val qualification = Qualification.text.toString()
+            val profession = Profession.text.toString()
+            val hobby = Hobby.text.toString()
+            val dreamjob = DreamJob.text.toString()
+
+//          Intent declaration:
+            val intent = Intent(this@MainActivity, UserInputDisplay::class.java)
+            intent.putExtra("Full Name: ", fullname)
+            intent.putExtra("Qualifications: ", qualification)
+            intent.putExtra("Profession: ", profession)
+            intent.putExtra("Hobbies: ", hobby)
+            intent.putExtra("Dream Job: ", dreamjob)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/exercise_1/UserInputDisplay.kt
@@ -0,0 +1,28 @@
+package com.example.exercise_1
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class UserInputDisplay : AppCompatActivity() {
+    @SuppressLint("SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_input_display)
+
+//      Intent declaration to receive data from the Main Activity
+        val intent = intent
+        val fname = intent.getStringArrayExtra("Full Name: ")
+        val qualif = intent.getStringArrayExtra("Qualifications: ")
+        val prof = intent.getStringArrayExtra("Profession: ")
+        val hobb = intent.getStringArrayExtra("Hobbies: ")
+        val dj = intent.getStringArrayExtra("Dream Job: ")
+
+//      Initialize the textview which is going to hold the data from Main activity
+        val datadisplay = findViewById<TextView>(R.id.InputDisplay)
+
+//      Display the user data in the textview
+        datadisplay.text = "Full Name: "+fname+"\nQualifications: "+qualif+"\nProfession: "+prof+"\nHobbies: "+hobb+"\nDream Job:"+dj
+    }
+}
