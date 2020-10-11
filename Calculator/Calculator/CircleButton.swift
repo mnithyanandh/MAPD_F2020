b3cba271a657ea6f340f4735a7cc1983d21469d2
mnithyanandh <71746055+mnithyanandh@users.noreply.github.com>
2020-10-11 16:50:24 +0530

    Assignment 2 completed
    Branch incremental commits avoided as XCode is bricking on not able to unlock project.

---
+++ Calculator/Calculator/CircleButton.swift
@@ -0,0 +1,32 @@
+//
+//  File Name: CircleButton.swift
+//  Created by Nithyanandh Mahalingam on 10/08/20
+//  Student ID: 301162314
+//  App: Calculator - Assignment 2
+//
+
+import UIKit
+
+@IBDesignable
+// IBAction
+class CircleButton: UIButton {
+    
+    // Initialize the function of the circle button and set its init stage as 'false' for this interface
+    @IBInspectable var circleButton:Bool = false{
+        
+        // Set the property of the circle button to crop the height
+        didSet{
+            if circleButton{
+                // Assigning the circle radius
+                layer.cornerRadius = frame.height / 2
+            }
+        }
+    }
+    
+    // Prepare an override function for the delcared circle Button that reverts to the interface declared above
+    override func prepareForInterfaceBuilder() {
+        if circleButton{
+            layer.cornerRadius = frame.height / 2
+        }
+    }
+}
