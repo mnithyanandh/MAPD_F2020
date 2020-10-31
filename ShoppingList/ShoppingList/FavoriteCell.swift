f17561372fb39476de3ab59879264c21329b47c7
mnithyanandh <71746055+mnithyanandh@users.noreply.github.com>
2020-10-31 08:50:40 +0530

    Buttons, Picker View, Picker Labels added.

--- ShoppingList/ShoppingList/FavoriteCell.swift
+++ ShoppingList/ShoppingList/FavoriteCell.swift
@@ -9,14 +9,14 @@ import UIKit
 
 class FavoriteCell: UITableViewCell {
     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
-        let fav = UIButton(type: .system)
-        fav.setTitle("THIS", for: .normal)
-        fav.setImage(fav_heart, for: .normal)
-        fav.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
-        accessoryView = fav
-        super.init(style: <#T##UITableViewCell.CellStyle#>, reuseIdentifier: <#T##String?#>)
-    }
+//    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?){
+////        let fav = UIButton(type: .system)
+////        fav.setImage(#imageLiteral(resourceName: "fav_heart"), for: .normal)
+////        fav.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+////        accessoryView = fav
+////        fav.tintColor = .red
+//         super.init(style: <#T##UITableViewCell.CellStyle#>, reuseIdentifier: <#T##String?#>)
+//    }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
--- ShoppingList/ShoppingList/ViewController.swift
+++ ShoppingList/ShoppingList/ViewController.swift
@@ -8,25 +8,168 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITabViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
-    // Creating an Outlet for the Table View:
-    @IBOutlet override var tableview : UITableView!
-
+    var message:String = "Do you wish to cancel without saving?"
+    
+    // Creating outlets for Picker Views:
+    @IBOutlet weak var pcView1: UIPickerView!
+    @IBOutlet weak var pcView2: UIPickerView!
+    @IBOutlet weak var pcView3: UIPickerView!
+    @IBOutlet weak var pcView4: UIPickerView!
+    @IBOutlet weak var pcView5: UIPickerView!
+    @IBOutlet weak var pcView6: UIPickerView!
+    
+    // Cerating outlets for Picker Labels:
+    @IBOutlet weak var pcLabel1: UILabel!
+    @IBOutlet weak var pcLabel2: UILabel!
+    @IBOutlet weak var pcLabel3: UILabel!
+    @IBOutlet weak var pcLabel4: UILabel!
+    @IBOutlet weak var pcLabel5: UILabel!
+    @IBOutlet weak var pcLabel6: UILabel!
+    
+    // Creating outlets for Item Text Fields:
+    @IBOutlet weak var listName: UITextField!
+    @IBOutlet weak var itemName1: UITextField!
+    @IBOutlet weak var itemName2: UITextField!
+    @IBOutlet weak var itemName3: UITextField!
+    @IBOutlet weak var itemName4: UITextField!
+    @IBOutlet weak var itemName5: UITextField!
+    @IBOutlet weak var itemName6: UITextField!
+    
+    // Cancel Button to rest all fields.
+    @IBAction func cancelButton(_ sender: UIButton) {
+        let alertController:UIAlertController = UIAlertController(title: "Cancel", message: message, preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "Yes", style: .default, handler:{action in
+            self.listName.text = ""
+            self.itemName1.text = ""
+            self.itemName2.text = ""
+            self.itemName3.text = ""
+            self.itemName4.text = ""
+            self.itemName5.text = ""
+            self.itemName6.text = ""
+        }))
+        alertController.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+        self.present(alertController, animated: true)
+    }
+    
+    // Declare arrays for PickerVIews:
+    let arr1 = [Int](0...499)
+    let arr2 = [Int](0...499)
+    let arr3 = [Int](0...499)
+    let arr4 = [Int](0...499)
+    let arr5 = [Int](0...499)
+    let arr6 = [Int](0...499)
+    
+    // Creating outlets for UI Buttons:
+    @IBOutlet weak var saveOutlet: UIButton!
+    @IBOutlet weak var cancelOutlet: UIButton!
+    @IBOutlet weak var favoritesOutlet: UIButton!
+    @IBOutlet weak var addmoreOutlet: UIButton!
+    
+    // Start of viewDidLoad:
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableview.delegate = self
-        tableview.dataSource = self
         
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
-    }
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Adding Picker View Delegates & Data Sources:
+        pcView1.delegate = self
+        pcView1.dataSource = self
+        pcView2.delegate = self
+        pcView3.delegate = self
+        pcView4.delegate = self
+        pcView5.delegate = self
+        pcView6.delegate = self
         
-        let cell1 = tableview.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
-        cell1.textLabel?.text = "Enter new List name:"
-        return cell1
+        pcView2.dataSource = self
+        pcView3.dataSource = self
+        pcView4.dataSource = self
+        pcView5.dataSource = self
+        pcView6.dataSource = self
+        
+        // Adding Shadows to buttons:
+        // Save Button:
+        saveOutlet.layer.shadowColor = UIColor.black.cgColor
+        saveOutlet.layer.shadowOffset = CGSize(width: 5, height: 5)
+        saveOutlet.layer.shadowRadius = 2
+        saveOutlet.layer.shadowOpacity = 0.6
+        
+        // Cancel Button:
+        cancelOutlet.layer.shadowColor = UIColor.black.cgColor
+        cancelOutlet.layer.shadowOffset = CGSize(width: 5, height: 5)
+        cancelOutlet.layer.shadowRadius = 2
+        cancelOutlet.layer.shadowOpacity = 0.6
+        
+        // Favorites Button:
+        favoritesOutlet.layer.shadowColor = UIColor.black.cgColor
+        favoritesOutlet.layer.shadowOffset = CGSize(width: 5, height: 5)
+        favoritesOutlet.layer.shadowRadius = 2
+        favoritesOutlet.layer.shadowOpacity = 0.6
+        
+        // Add More Button:
+        addmoreOutlet.layer.shadowColor = UIColor.black.cgColor
+        addmoreOutlet.layer.shadowOffset = CGSize(width: 5, height: 5)
+        addmoreOutlet.layer.shadowRadius = 2
+        addmoreOutlet.layer.shadowOpacity = 0.6
+    }
+    
+    override func didReceiveMemoryWarning() {
+        // Dispose of any resources that can be recreated.
+        super.didReceiveMemoryWarning()
+    }
+    
+    // PickerView Method - numberOfComponents:
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // PickerView Method - numberOfRowsInComponent:
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pcView1.tag == 1{
+            return arr1.count
+        }else if pcView2.tag == 2{
+            return arr2.count
+        }else if pcView3.tag == 3{
+            return arr3.count
+        }else if pcView4.tag == 4{
+            return arr4.count
+        }else if pcView5.tag == 5{
+            return arr5.count
+        }else{
+            return arr6.count
+        }
+    }
+    
+    // PickerView Method - titleForRow:
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pcView1.tag == 1{
+            return "\(arr1[row])"
+        }else if pcView2.tag == 2{
+            return "\(arr2[row])"
+        }else if pcView3.tag == 3{
+            return "\(arr3[row])"
+        }else if pcView4.tag == 4{
+            return "\(arr4[row])"
+        }else if pcView5.tag == 5{
+            return "\(arr5[row])"
+        }else{
+            return "\(arr6[row])"
+        }
+    }
+    
+    // PickerView Method - didSelectRow:
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pcView1.tag == 1{
+            return pcLabel1.text = "\(arr1[row])"
+        }else if pcView2.tag == 2{
+            return pcLabel2.text = "\(arr2[row])"
+        }else if pcView3.tag == 3{
+            return pcLabel3.text = "\(arr3[row])"
+        }else if pcView4.tag == 4{
+            return pcLabel4.text = "\(arr4[row])"
+        }else if pcView5.tag == 5{
+            return pcLabel5.text = "\(arr5[row])"
+        }else{
+            return pcLabel6.text = "\(arr6[row])"
+        }
     }
 }
-
