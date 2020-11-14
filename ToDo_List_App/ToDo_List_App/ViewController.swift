6ba7ea9e91e169b81ff409d1f0a79506e10c705f
mnithyanandh <71746055+mnithyanandh@users.noreply.github.com>
2020-11-14 17:17:45 +0530

    Base Commit - ToDo List App
    Xcode integration

---
+++ ToDo_List_App/ToDo_List_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo_List_App
+//
+//  Created by Nithyanandh Mahalingam on 11/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
