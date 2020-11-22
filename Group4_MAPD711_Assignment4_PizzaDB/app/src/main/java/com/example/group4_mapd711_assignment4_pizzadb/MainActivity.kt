bd827bfbe7b902a54a02e171029d77775d6ea9b1
mnithyanandh <71746055+mnithyanandh@users.noreply.github.com>
2020-11-22 19:47:01 +0530

    Assignment 4  - Base commit

---
+++ Group4_MAPD711_Assignment4_PizzaDB/app/src/main/java/com/example/group4_mapd711_assignment4_pizzadb/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.group4_mapd711_assignment4_pizzadb
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
