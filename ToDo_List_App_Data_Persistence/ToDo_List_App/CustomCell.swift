310324b098af6ccc6b8752c4ad8f652bb0a8064c
mnithyanandh <71746055+mnithyanandh@users.noreply.github.com>
2020-11-30 02:44:17 +0530

    Clone of Assignment4 made - Base Commit

---
+++ ToDo_List_App_Data_Persistence/ToDo_List_App/CustomCell.swift
@@ -0,0 +1,18 @@
+//  File name: CustomCell.swift
+//
+//  Author's name: Created by Nithyanandh Mahalingam on 11/14/20.
+//  StudentID: 301162314
+//  Date: 11/14/20
+//  App description: ToDo_List_App
+//
+
+import Foundation
+
+import UIKit
+class CustomCell: UITableViewCell {
+
+    @IBOutlet weak var TaskNameLbl: UILabel!
+    @IBOutlet weak var DueDateLbl: UILabel!
+    @IBOutlet weak var TaskActiveSwitch: UISwitch!
+    @IBOutlet weak var TaskEditBtn: UIButton!
+}
---
+++ ToDo_List_App_Data_Persistence/ToDo_List_App/ViewController.swift
@@ -0,0 +1,52 @@
+//  File name: ViewController.swift
+//
+//  Author's name: Created by Nithyanandh Mahalingam on 11/14/20.
+//  StudentID: 301162314
+//  Date: 11/14/20
+//  App description: ToDo_List_App
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var user_tasks =  [String]()
+    var user_task_due = [Date]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBOutlet weak var UITableViewOutlet: UITableView!
+    @IBAction func AddMoreTasks(_ sender: UIButton) {
+    }
+    @IBAction func DeleteTasks(_ sender: UIButton) {
+    }
+}
+
+extension ViewController: UITableViewDelegate{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+extension ViewController: UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return user_tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let dateformat = DateFormatter()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CustomCell", for: indexPath) as! CustomCell
+        cell.TaskNameLbl.text = user_tasks[indexPath.row]
+        cell.DueDateLbl.text = dateformat.string(from: user_task_due[indexPath.row])
+        if(cell.TaskNameLbl.text != "Task 1")
+        {
+            cell.TaskActiveSwitch.isOn = true
+        }
+        else{
+            cell.TaskActiveSwitch.isOn = false
+        }
+        
+        return cell
+    }
+}
+
