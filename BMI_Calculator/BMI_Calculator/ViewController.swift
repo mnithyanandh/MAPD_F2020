9512d315c74cb50b24ec6104e2b03a8ccf76ea91
mnithyanandh <71746055+mnithyanandh@users.noreply.github.com>
2020-12-12 05:46:53 +0530

    Final Project Base commit

---
+++ BMI_Calculator/BMI_Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI_Calculator
+//
+//  Created by Nithyanandh Mahalingam on 12/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
